2409dfdc7435587221961d10310802bd7cb5c2f0
mohibuslc <mohib_uslc@outlook.com>
2021-03-21 21:07:50 +0000

    Recap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React , {useState} from 'react'; 
 function App() {
+  const ActorData = ['Tina', 'Jina','Minia','Tamanna-Begum','Deepjol-Caca']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieCounter></MovieCounter>
+
+      <Nayok name="Josim"></Nayok>
+      <Nayok name = {ActorData[4]}></Nayok>
+      <Nayok name ="Rubal"></Nayok>
+      <Nayok name ="Bappa Raj"></Nayok>
+      <Nayok name = {ActorData [3]}></Nayok>
     </div>
   );
 }
+function MovieCounter(){
+  const [count, setCount] = useState(4); // Use useState:
+    const handler = () => {
 
+      const AddMovie = count+1;
+      setCount(AddMovie);
+    }
+    const handlerDedact = () => {
+
+      const DedactMovie = count-1;
+      setCount(DedactMovie);
+    }
+  
+  return(
+    <div>
+      <button onClick = {handler} style= {{marginTop:'24px'}}>Movie-Counter</button>
+      <button onClick = {handlerDedact}>Movie-Decrase</button>
+      <h3>Number Of Movie :{count} </h3>
+
+      
+    </div>
+
+
+
+
+  )
+}
+
+function Nayok(props){
+const nayokstyle ={
+  border:'2px solid red',
+  margin: '10px',
+ 
+
+
+
+}
+  return(    
+  <div style={nayokstyle } >
+  <h1 style={{color:'yellowgreen'}}>I M Actores . / Actress :{props.name} </h1> 
+  <h3>Bangladesh FDC</h3>
+  </div> 
+
+   )
+}
 export default App;
